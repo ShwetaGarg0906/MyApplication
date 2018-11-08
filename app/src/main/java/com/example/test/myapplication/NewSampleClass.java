5eb66e2e1aa49ed8bce7f0557fb02329c46ba60b
shweta garg <shwetagarg0906@gmail.com>
2018-11-08 15:04:12 +0530

    added new java class SampleClass

--- app/src/main/java/com/example/test/myapplication/NewSampleClass.java
+++ app/src/main/java/com/example/test/myapplication/NewSampleClass.java
@@ -1,4 +1,9 @@
 package com.example.test.myapplication;
 
+import android.util.Log;
+
 public class NewSampleClass {
+    public static void main(){
+        Log.d("message","file committed");
+    }
 }
